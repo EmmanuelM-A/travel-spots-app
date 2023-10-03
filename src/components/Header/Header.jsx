31ac412fb927956afc74f2bea0895a060aca6d73
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2023-10-03 12:56:42 +0100

    added the the appbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,33 @@
-import Reeact from 'react';
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, ToolBar, Typography, InputBase, Box } from '@material-ui/core';
+import { SearchIcon } from '@material-ui/icons/Search';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
+        <>
+            <AppBar position="static">
+                <ToolBar className={classes.toolbar}>
+                    <Typography variant="h5" className={classes.title}>
+                        Travel Spots
+                    </Typography>
+                    <Box display="flex">
+                        <Typography variant="h6" className={classes.title}>
+                            Explore new places
+                        </Typography>
+                        <Autocomplete>
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
+                                    <SearchIcon />
+                                </div>
+                                <InputBase placeholder="Search..." classes={{root: classes.rootInput, input: classes.inputInput}} />
+                            </div>
+                        </Autocomplete>
+                    </Box>
+                </ToolBar>
+            </AppBar>
+        </>
+    );
 }
 
 export default Header;
