058ac603f0da35cb5d6a981da69191689709c941
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2023-10-03 13:40:42 +0100

    made the code more readable

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -32,16 +32,19 @@ export default makeStyles((theme) => ({
         justifyContent: 'center',
     },
     inputRoot: {
-      color: 'inherit',
+        color: 'inherit',
     },
     inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: { width: '20ch' },
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: { 
+            width: '20ch' 
+        },
     },
     toolbar: {
-      display: 'flex', justifyContent: 'space-between',
+      display: 'flex',
+      justifyContent: 'space-between',
     },
 }));
